c22fcf1835db18dd7f924b4dbdefdddc927e3c34
Ryan L <fartman553@gmail.com>
2023-02-03 21:22:04 -0500

    bootstrap stuff

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template, request, url_for, flash 
+from flask import Flask, redirect, render_template, request, url_for, flash
 from flask import get_flashed_messages
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date, datetime
@@ -11,61 +11,53 @@ import sqlite3
 
 app = Flask(__name__)
 
-with open ("appPW.txt") as f: #to hide and open the secret key
+with open("appPW.txt") as f:  # to hide and open the secret key
     passW = f.readline()
-    
-    
-app.config['SECRET_KEY'] = passW
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.db'
+
+
+app.config["SECRET_KEY"] = passW
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.db"
 db = SQLAlchemy(app)
-
-
-conn = sqlite3.connect("instance/db.db", check_same_thread=False) #Making a connection to the database
-
-cursor = conn.cursor()
-query = 'SELECT * FROM student'
-
-cursor.execute(query) #Execute this query
-conn.commit()
-students = cursor.fetchall()  # fetches all (or all remaining) rows of a query result set and returns a list of tuples.
-
-
 
 
 class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    fullName = db.Column(db.String(80), nullable=False) #To make the colmun not contain a NULL value.
+    fullName = db.Column(
+        db.String(80), nullable=False
+    )  # To make the colmun not contain a NULL value.
     age = db.Column(db.Integer, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    date_created = db.Column(db.DateTime, default = datetime.utcnow)
-    
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+
 class TA(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     fullName = db.Column(db.String(80), nullable=False)
     age = db.Column(db.Integer, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    date_created = db.Column(db.DateTime, default = datetime.utcnow)
-    
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
 
 class MyForm(FlaskForm):
-    fullName = StringField('Enter Name: ', validators=[DataRequired()])
-    age = StringField('Enter age', validators=[DataRequired()])
-    email = StringField('Enter email', validators=[DataRequired()])
+    fullName = StringField("Enter Name: ", validators=[DataRequired()])
+    age = StringField("Enter age", validators=[DataRequired()])
+    email = StringField("Enter email", validators=[DataRequired()])
     submit = SubmitField("Register Here")
 
-    
+
 class TAForm(FlaskForm):
-    fullName = StringField('Enter Name: ', validators=[DataRequired()])
-    age = StringField('Enter age', validators=[DataRequired()])
-    email = StringField('Enter email', validators=[DataRequired()])
+    fullName = StringField("Enter Name: ", validators=[DataRequired()])
+    age = StringField("Enter age", validators=[DataRequired()])
+    email = StringField("Enter email", validators=[DataRequired()])
     submit = SubmitField("Register Here")
-    
- 
-@app.route('/')
-def index():
-    return render_template('main.html')
 
-@app.route('/register', methods=["GET", "POST"]) #The main page route
+
+@app.route("/")
+def index():
+    return render_template("main.html")
+
+
+@app.route("/register", methods=["GET", "POST"])  # The main page route
 def register():
     student_form = MyForm()
     ta_form = TAForm()
@@ -73,39 +65,61 @@ def register():
         student_register()
     elif ta_form.validate_on_submit():
         ta_register()
-    
-    return render_template('register.html', student_form=student_form, ta_form=ta_form)
+
+    return render_template("register.html", student_form=student_form, ta_form=ta_form)
 
 
-@app.route('/student_register', methods=["GET","POST"])
+@app.route("/student_register", methods=["GET", "POST"])
 def student_register():
-    form = MyForm() 
+    form = MyForm()
     if form.validate_on_submit():
         flash("Record successfully added.")
-        new_student = Student(fullName=form.fullName.data, age=form.age.data, email = form.email.data)
-        cursor.execute("INSERT INTO student (fullName, age, email) VALUES (?, ?, ?)", (new_student.fullName, new_student.age, new_student.email))
+        new_student = Student(
+            fullName=form.fullName.data, age=form.age.data, email=form.email.data
+        )
+        conn = sqlite3.connect("instance/db.db", check_same_thread=False)
+
+        cursor = conn.cursor()
+        cursor.execute(
+            "INSERT INTO student (fullName, age, email) VALUES (?, ?, ?)",
+            (new_student.fullName, new_student.age, new_student.email),
+        )
+        res = cursor.execute("SELECT * FROM student")
+        global students
+        students = res.fetchall()
         conn.commit()
-        return redirect(url_for('register'))
-    
-    
-@app.route('/ta_register', methods=["GET","POST"])
+        conn.close()
+        return redirect(url_for("register"))
+
+
+@app.route("/ta_register", methods=["GET", "POST"])
 def ta_register():
     ta_form = TAForm()
     if ta_form.validate_on_submit():
         flash("Record successfully added.")
-        new_TA = TA(fullName=ta_form.fullName.data, age=ta_form.age.data, email=ta_form.email.data)
-        cursor.execute("INSERT INTO TA (fullName, age, email) VALUES (?, ?, ?)", (new_TA.fullName, new_TA.age, new_TA.email))
+        new_TA = TA(
+            fullName=ta_form.fullName.data,
+            age=ta_form.age.data,
+            email=ta_form.email.data,
+        )
+        conn = sqlite3.connect("instance/db.db", check_same_thread=False)
+        cursor = conn.cursor()
+        cursor.execute(
+            "INSERT INTO TA (fullName, age, email) VALUES (?, ?, ?)",
+            (new_TA.fullName, new_TA.age, new_TA.email),
+        )
+        global ta
+        res = cursor.execute("SELECT * FROM ta")
+        ta = res.fetchall()
         conn.commit()
-        return redirect(url_for('register'))
-    
-    
-@app.route('/test')
-def test():
-     return render_template('test.html', students=students)
-    
-    
-    
+        conn.close()
+        return redirect(url_for("register"))
 
-    
-if __name__ == '__main__':
+
+@app.route("/test")
+def test():
+    return render_template("test.html", students=students)
+
+
+if __name__ == "__main__":
     app.run(debug=True)
